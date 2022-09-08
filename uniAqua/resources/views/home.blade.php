5a5fadb8177278b2f3244b7ea0b05683aaea68ba
“Cal-107” <kaly___7@hotmail.it>
2022-09-08 14:24:14 +0200

    add initial setup

---
+++ uniAqua/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('title', '- Home')
+    
+@section('content')
+<section class="vh-100 container d-flex justify-content-center align-items-center">
+    <h1>
+        Welcome Guest
+    </h1>
+</section>
+@endsection
---
+++ uniAqua/resources/views/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid w-75">
+        {{-- <div>
+            <img src="//airport/public/images/aquaconsulting-logo.png" alt="Logo Aqua">
+        </div> --}}
+        <a class="navbar-brand" href="#">Aqua University</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flez justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Degree Course</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Students</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
